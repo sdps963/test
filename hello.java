2dedc6e4a153e9c3911f7bbadc8c1f3960192ed9
sdps963 <sdps963@gmail.com>
2018-02-25 22:13:09 -0600

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 public class Hello{
 
-public static void main(String[] args){
+    public static void main(String[] args){
 
-    for(int i=1;i<=10;i++){
-      System.out.println("Hello World" +i);
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello World" +i);
     }
   }
 }
